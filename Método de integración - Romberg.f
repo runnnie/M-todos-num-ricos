2d0840ce78048b521a0a820c12fc03dad31da577
Brayan Martínez <brayanelgamesa@hotmail.com>
2023-02-02 13:21:02 -0600

    Se agrego la opción de detenerse cuando detecta cierto error digitado por el usuario

--- Método de integración - Romberg.f
+++ Método de integración - Romberg.f
@@ -5,7 +5,7 @@
 
       integer i,j
       integer k
-      real a,b,h
+      real a,b,e_calculado,e_requerido,abs
       double precision f,fsuma
       double precision , allocatable,dimension(:,:)::R
 
@@ -13,43 +13,84 @@
       read(*,*)k
       allocate(R(k,k))
 
-      write(*,*)"Digite el l¡mite a: "
+      write(*,*)"Digite el l¡mite a: "     !Le pregunta al usuario los limites de integraci¢n
       read(*,*)a
       write(*,*)"Digite el l¡mite b:"
       read(*,*)b
 
-
-      h = b-a
-      R(1,1) = h*0.5*(f(a) + f(b))
-
+      write(*,*)"Ingrese el error requerido: "
+      read(*,*)e_requerido
 
 
-      do i=2,k
+
+      R(1,1) = (b-a)*0.5*(f(a) + f(b))
+
+
+
+
+      e_calculado = 34 !Le asigna un valor arbitrario al error, para luego poder compararlo con el error digitado
+
+
+
+
+      !Esta parte se encarga de calcular los elementos de la primera columna de la matriz
+      do i=1,k
+      if (i>=2) then
       R(i,1) = 0.5*(R(i-1,1) + (b-a)/(2**(i-2))*fsuma(i,a,b))
-      end do
-
-      do i=1,k
-      write(*,*)R(i,1)
-      end do
-
-      !Extrapolacion de richarson
-
-      do i=1,k
-      do j=2,k
-
-      if (j<=i) then
-
-
-      R(i,j) = (4**(j-1)*R(i,j-1)-R(i-1,j-1))/(4**(j-1)-1)
-
       end if
 
 
 
+      do j=2,k
+
+      !La condici¢n if sirve para seleccionar solo aquellos elementos que esten debajo de la matriz triangular inferior
+      if (j<=i) then
+      R(i,j) = (4**(j-1)*R(i,j-1)-R(i-1,j-1))/(4**(j-1)-1)
+
+
+      write(*,*)i,j
+      e_calculado = abs((R(i,j) - R(i,j-1))/R(i,j))
+      write(*,*)"El error es: ",e_calculado
+
+
+
+      !Le pregunta al usuario si el error calculado es menor o igual al error requerido
+      if(e_calculado <= e_requerido) then
+      exit
+
+
+      end if
+      end if
+
+
+
+
+      end do
+
+      if(e_calculado <= e_requerido) then
+      exit
+      end if
+
+      end do
+
+
+
+      !imprime los elementos de la matriz triangular inferior
+      do i=1,k
+      do j=1,k
+
+      if (j<=i) then
+
+      write(*,*)R(i,j)
+
+
+      end if
+
+
       end do
       end do
 
-      write(*,*)R(k,k)
+
 
       pause
       end program problema1
@@ -94,5 +135,29 @@
 
 
       !//////////////////////////////////////////////
+      real function abs(num)
+      real num
+
+      if(num<0) then
+
+      num = -num
+
+      end if
+      if(num>0) then
+
+      num = num
+      end if
+
+      abs = num
+
+      return
+      end function
+
+
+      !//////////////////////////////////////////////
+
+
+
+
 
 
